80710024c80a5a73435255869e9fb6b879b53ddc
fanggang <fanggang@huluwa.cc>
2018-05-19 00:33:50 +0800

    login

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -10,26 +10,48 @@ use yii\bootstrap\ActiveForm;
 $this->title = '注册';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-signup">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-<!--    <p>Please fill out the following fields to signup:</p>-->
-    <p>请输入以下信息完成注册</p>
+<div style="background-image:url(nanhuadaxuexiaomen.jpg);max-height: 300px">
+
     <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+        <div class="col-md-5">
 
-                <?= $form->field($model, 'email') ?>
-
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton('注册', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
         </div>
+
+        <div class="col-md-7 panel">
+
+            <div class="site-signup">
+                <h1><?= Html::encode($this->title) ?></h1>
+
+                <!--    <p>Please fill out the following fields to signup:</p>-->
+                <p>请输入以下信息完成注册</p>
+                <div class="row">
+                    <div class="col-lg-5">
+                        <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+
+                        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+                        <?= $form->field($model, 'email') ?>
+
+                        <?= $form->field($model, 'password')->passwordInput() ?>
+
+                        <div class="form-group">
+                            <?= Html::submitButton('注册', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
+                        </div>
+
+                        <?php ActiveForm::end(); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
     </div>
+
+
+
 </div>
+
+
