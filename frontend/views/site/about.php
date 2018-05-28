98585a6cfaac09243756515f1113b67dedbcf195
fangkemi <facck@sina.com>
2018-05-28 11:54:36 +0800

    Update about.php

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -8,9 +8,9 @@ $this->title = '关于';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <h1><?= Html::encode('关于本考试测试系统') ?></h1>
+    <h1><?= Html::encode('本考试测试系统面向矿山企业') ?></h1>
 
-    <p>本考试测试系统为施工安全员在线考核系统，提供在线模拟测试和真实测试功能</p>
+    <p>本考试测试系统为施工安全员在线考核系统，提供在线模拟测试和真实测试考核功能</p>
 
 <!--    <code>--><?//= __FILE__ ?><!--</code>-->
 </div>
